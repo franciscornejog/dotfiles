d7a0aa8f0c9963f42938a11dd59631b6a2a48a1c
Francisco Cornejo-Garcia <franciscornejog@gmail.com>
2021-11-06 03:00:39 -0400

    Refactor map into a function with sane default

--- nvim/nvim/lua/maps.lua
+++ nvim/nvim/lua/maps.lua
@@ -1,17 +1,19 @@
-local map = vim.api.nvim_set_keymap
-local options = { noremap = true }
+local map = function(mode, lhs, rhs)
+    local options = { noremap = true }
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
 
 vim.g.mapleader = ' '
 map('n', ' ', '', {})
-map('n', ' ve', ':edit ~/.config/nvim/init.lua<cr>', options)
-map('n', 'n', 'nzzzv', options)
-map('n', 'N', 'Nzzzv', options)
-map('n', 'J', 'mjJ`j', options)
+map('n', ' ve', ':edit ~/.config/nvim/init.lua<cr>')
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+map('n', 'J', 'mjJ`j')
 
-map('t', '<C-w>N', '<C-\\><C-N>', options)
-map('t', '<C-w>h', '<C-\\><C-N><C-w>h', options)
-map('t', '<C-w>j', '<C-\\><C-N><C-w>j', options)
-map('t', '<C-w>k', '<C-\\><C-N><C-w>k', options)
-map('t', '<C-w>l', '<C-\\><C-N><C-w>l', options)
+map('t', '<C-w>N', '<C-\\><C-N>')
+map('t', '<C-w>h', '<C-\\><C-N><C-w>h')
+map('t', '<C-w>j', '<C-\\><C-N><C-w>j')
+map('t', '<C-w>k', '<C-\\><C-N><C-w>k')
+map('t', '<C-w>l', '<C-\\><C-N><C-w>l')
 
 
