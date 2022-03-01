bbfe7269c48bb23061df85a21b4f39887c50acf3
Francisco Cornejo-Garcia <franciscornejog@gmail.com>
2022-03-01 08:46:08 -0800

    Update keymaps

--- neovim/nvim/lua/plugins.lua
+++ neovim/nvim/lua/plugins.lua
@@ -7,24 +7,22 @@ end
 return require('packer').startup(function(use)
     use { 'wbthomason/packer.nvim',
         config = function()
-            vim.keymap.set('n', ' ps', ':PackerSync<cr>')
+            vim.keymap.set('n', ' rs', ':PackerSync<cr>')
         end }
     use 'franciscornejog/noired.nvim'
     use 'nvim-lua/plenary.nvim'
     use { 'norcalli/nvim-colorizer.lua',
         config = function()
-            vim.keymap.set('n', ' ct', ':ColorizerToggle<cr>')
+            vim.keymap.set('n', ' tc', ':ColorizerToggle<cr>')
         end }
     use { 'nvim-telescope/telescope.nvim',
         config = function()
-            vim.keymap.set('n', ' ff', ":lua require('telescope.builtin').find_files()<cr>")
-            vim.keymap.set('n', ' fb', ":lua require('telescope.builtin').file_browser()<cr>")
-            vim.keymap.set('n', ' fg', ":lua require('telescope.builtin').live_grep()<cr>")
-            vim.keymap.set('n', ' gf', ":lua require('telescope.builtin').git_files()<cr>")
-            vim.keymap.set('n', ' vb', ":lua require('telescope.builtin').buffers()<cr>")
-            vim.keymap.set('n', ' vh', ":lua require('telescope.builtin').help_tags()<cr>")
-            vim.keymap.set('n', ' vk', ":lua require('telescope.builtin').keymaps()<cr>")
-            vim.keymap.set('n', ' vl', ":lua require('telescope.builtin').highlights()<cr>")
+            vim.keymap.set('n', ' sf', ":lua require('telescope.builtin').find_files()<cr>")
+            vim.keymap.set('n', ' st', ":lua require('telescope.builtin').live_grep()<cr>")
+            vim.keymap.set('n', ' sg', ":lua require('telescope.builtin').git_files()<cr>")
+            vim.keymap.set('n', ' sb', ":lua require('telescope.builtin').buffers()<cr>")
+            vim.keymap.set('n', ' sk', ":lua require('telescope.builtin').keymaps()<cr>")
+            vim.keymap.set('n', ' sh', ":lua require('telescope.builtin').highlights()<cr>")
         end }
     use { 'nvim-treesitter/nvim-treesitter',
         config = function()
@@ -37,8 +35,8 @@ return require('packer').startup(function(use)
         run = ':TSUpdate' }
     use { 'nvim-treesitter/playground',
         config = function()
-            vim.keymap.set('n', ' tt', ':TSPlaygroundToggle<cr>')
-            vim.keymap.set('n', ' tl', ':TSHighlightCapturesUnderCursor<cr>')
+            vim.keymap.set('n', ' tp', ':TSPlaygroundToggle<cr>')
+            vim.keymap.set('n', ' ph', ':TSHighlightCapturesUnderCursor<cr>')
         end }
     use { 'lewis6991/gitsigns.nvim',
         config = function()
@@ -83,17 +81,14 @@ return require('packer').startup(function(use)
         end }
     use { 'neovim/nvim-lspconfig',
         config = function()
-            vim.keymap.set('n', ' vd', ':lua vim.lsp.buf.definition()<cr>')
-            vim.keymap.set('n', ' vD', ':lua vim.lsp.buf.declaration()<cr>')
-            vim.keymap.set('n', ' vi', ':lua vim.lsp.buf.implementation()<cr>')
-            vim.keymap.set('n', ' vsh', ':lua vim.lsp.buf.signature_help()<cr>')
-            vim.keymap.set('n', ' vrr', ':lua vim.lsp.buf.references()<cr>')
-            vim.keymap.set('n', ' vrn', ':lua vim.lsp.buf.rename()<cr>')
-            vim.keymap.set('n', ' vh', ':lua vim.lsp.buf.hover()<cr>')
-            vim.keymap.set('n', ' vca', ':lua vim.lsp.buf.code_action()<cr>')
-            vim.keymap.set('n', ' vds', ':lua vim.lsp.diagnostic.show_line_diagnostics()<cr>')
-            vim.keymap.set('n', ' vdn', ':lua vim.lsp.diagnostic.goto_next()<cr>')
-            vim.keymap.set('n', ' vdp', ':lua vim.lsp.diagnostic.goto_prev()<cr>')
+            vim.keymap.set('n', ' gd', ':lua vim.lsp.buf.definition()<cr>')
+            vim.keymap.set('n', ' gD', ':lua vim.lsp.buf.declaration()<cr>')
+            vim.keymap.set('n', ' gi', ':lua vim.lsp.buf.implementation()<cr>')
+            vim.keymap.set('n', ' gr', ':lua vim.lsp.buf.references()<cr>')
+            vim.keymap.set('n', ' rr', ':lua vim.lsp.buf.rename()<cr>')
+            vim.keymap.set('n', ' ra', ':lua vim.lsp.buf.code_action()<cr>')
+            vim.keymap.set('n', ' gn', ':lua vim.lsp.diagnostic.goto_next()<cr>')
+            vim.keymap.set('n', ' gp', ':lua vim.lsp.diagnostic.goto_prev()<cr>')
             local installation_path = '/usr/local/Cellar/lua-language-server/2.5.3/libexec'
             local binary = installation_path .. '/bin/macOS/lua-language-server'
             local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -122,10 +117,14 @@ return require('packer').startup(function(use)
         config = function()
             require('rust-tools').setup()
         end }
+    use { 'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end }
     use { '~/Documents/projects/test-plugin',
         config = function()
             require('test-plugin').setup()
-            vim.keymap.set('n', ' tp', ":lua require('test-plugin').test()<cr>")
+            vim.keymap.set('n', ' rt', ":lua require('test-plugin').test()<cr>")
         end }
     -- use 'mfussenegger/nvim-jdtls'
 end)
