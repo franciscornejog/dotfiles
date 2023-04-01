cea766decd83952314863719b80500e2a3d7b27f
Francisco Cornejo-Garcia <franciscornejog@gmail.com>
2023-04-01 16:49:59 -0400

    Change filename

--- neovim/nvim/lua/git-nvim.lua
+++ neovim/nvim/lua/git-nvim.lua
@@ -1,4 +1,9 @@
-return { 'lewis6991/gitsigns.nvim', 'tpope/vim-fugitive', config = function()
-    require('gitsigns').setup()
-end }
+return { 'lewis6991/gitsigns.nvim', 'tpope/vim-fugitive',
+    config = function()
+        require('gitsigns').setup()
+        vim.keymap.set('n', ' gl', ':Git log<cr>')
+        vim.keymap.set('n', ' gc', ':Git commit<cr>')
+        vim.keymap.set('n', ' gp', ':Git push<cr>')
+    end
+}
 
